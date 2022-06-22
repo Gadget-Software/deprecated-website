fa09c9963914a4006628b56e3643d896de81ec3e
greduan <me@greduan.com>
2022-06-22 18:15:27 +0200

    fix (fixate) hero background image size

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Parallax } from 'react-parallax';
+import './style.scss';
 import image from '../../assets/img/gadget_software.jpg';
 
 export const Hero = ({ title, small = false }) => {
@@ -10,13 +11,12 @@ export const Hero = ({ title, small = false }) => {
       'h-screen': !small,
       'h-screen/2': small,
     },
-    'bg-black flex items-center justify-center bg-fixed bg-center bg-cover md:bg-contain bg-no-repeat',
+    'bg-black flex items-center justify-center bg-center bg-no-repeat bg-image',
   );
-  const image = require('../../assets/img/gadget_software.jpg');
   return (
     <div className="relative">
       <Parallax className={classes} bgImage={image} strength={400} />
-      <div className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-25 text-white">
+      <div className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-30 text-white">
         <div className="absolute top-1/2 text-4xl w-full md:max-w-2xl text-center px-10 md:text-left md:pl-10">
           {title}
         </div>
